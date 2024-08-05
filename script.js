d2a612d38eecabf8ce8787ba3eb759f7c384838b
RafaelAHoffmann <148063811+RafaelAHoffmann@users.noreply.github.com>
2024-08-05 12:10:54 -0300

    .

--- script.js
+++ script.js
@@ -95,9 +95,9 @@ mostraPergunta()
 function
 mostraAlternativas()
 {
-    for(const alternativas of perguntaAtual.alternativas)
+    for(const alternativa of perguntaAtual.alternativas)
     {
-        const alternativa = document.createElement("button");
+        const botaoAlternativas = document.createElement("button");
         botaoAlternativas.textContent = alternativa.texto;
         botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
